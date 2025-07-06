aadcb0395cc46849bbad5e56ff37b28f2f97455d
elitebot47 <elitebot47@gmail.com>
2025-07-06 21:29:42 +0530

    Add ContactForm component with validation; update ContactPage to use async function and integrate new form components for upcoming contact form feature

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,6 @@
 import ContactProfilesCard from "@/components/contactPage/contact-profiles-card";
 
-export default function ContactPage() {
+export default async function ContactPage() {
   return (
     <div className="w-full lg:py-40 flex flex-col items-center h-full lg:px-80 px-3  pt-16  ">
       <ContactProfilesCard />
