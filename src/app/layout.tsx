4e6d5b71fd99acd16f893220ae8d62deaec79721
elitebot47 <elitebot47@gmail.com>
2025-07-07 15:47:17 +0530

    fix padding below footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,7 @@ export default async function RootLayout({
             </footer>
           </div>
         </NextThemesProvider>
-        <Analytics />;
+        <Analytics />
       </body>
     </html>
   );
