44dd972fcc09f05d38bf18dca60839ddce1bb75d
elitebot47 <elitebot47@gmail.com>
2025-07-10 23:54:43 +0530

    feat: add metadata for WorkPage to enhance SEO and description

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,5 +1,11 @@
 import ProjectList from "@/components/workPage/projects-list";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Rishab Yadav | My Projects & Work",
+  description:
+    "Explore the modern web applications and projects built by Rishab Yadav, showcasing expertise in Next.js, TypeScript, React, and more.",
+};
 export default async function WorkPage() {
   return (
     <div className="w-full h-full  flex justify-center relative px-2 scroll-smooth scrollbar-none  lg:pt-32 pb-32 lg:pb-12 pt-24">
