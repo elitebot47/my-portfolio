75a282ddc5333ec64359b7bf5822769723a0f466
rishabyadav-dev <elitebot47@gmail.com>
2025-07-17 23:17:49 +0530

    Replace Cloudinary with local profile image

--- src/components/homePage/profile-picture-side/profile-picture-area.tsx
+++ src/components/homePage/profile-picture-side/profile-picture-area.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { motion } from "motion/react";
-import { CldImage } from "next-cloudinary";
+import Image from "next/image";
 
 export default function ProfilePictureArea() {
   return (
@@ -11,21 +11,17 @@ export default function ProfilePictureArea() {
       className="h-full lg:pl-12 mb-1 w-full flex-col flex justify-center items-center"
     >
       <div className="w-[170px] border-2 flex justify-center items-center rounded-full h-[170px] overflow-hidden">
-        <CldImage
-          alt="profile picture"
+        <Image
           width={1000}
           height={1000}
-          loading="lazy"
-          src="ihrnwy6eepwn2rx3wktq"
-          crop="auto"
-          gravity="face"
-          className="w-full h-full object-cover transition-transform duration-300 ease-in-out scale-150"
-        />
+          priority
+          className="w-full h-full hover:scale-150 ease-in-out transition-transform duration-500  scale-[140%]  object-cover  "
+          alt="profile picture"
+          src={`/images/profile.webp`}
+        ></Image>
       </div>
 
-      <div className="flex md:mt-6 mt-2.5 justify-center text-xl  items-center">
-        Gurgaon/India
-      </div>
+      <div className="flex md:mt-6 mt-2.5 justify-center text-xl  items-center">Gurgaon/India</div>
     </motion.div>
   );
 }
